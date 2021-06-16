83985569a091dc1f0a9ce9f4dbf90a1095ee46c1
ddhanushka <dimuthudhanushka8@gmail.com>
2021-06-16 17:21:26 +0530

    Search and clear

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import "./App.css";
 import Navbar from "./components/layout/Navbar";
 import Users from "./components/users/Users";
+import { Search } from "./components/users/Search";
 import axios from "axios";
 
 class App extends Component {
@@ -10,19 +11,29 @@ class App extends Component {
     loading: false,
   };
 
-  async componentDidMount() {
-    this.setState({ lading: true });
-    const res = await axios.get("https://api.github.com/users");
+  // search github users
+  searchUsers = async (text) => {
+    this.setState({ loading: true });
+    const res =
+      await axios.get(`https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET} = '41e6bbc8ac5349e10833'
+      }`);
 
-    this.setState({ users: res.data, loading: false });
-  }
+    this.setState({ users: res.data.items, loading: false });
+  };
+
+  clearUsers = () => this.setState({ users: [], loading: false });
 
   render() {
     return (
       <div>
         <Navbar />
         <div className="container">
-          <Users lading={this.state.loading} users={this.state.users} />
+          <Search
+            searchUsers={this.searchUsers}
+            clearUsers={this.clearUsers}
+            showClear={this.state.users.length > 0 ? true : false}
+          />
+          <Users loading={this.state.loading} users={this.state.users} />
         </div>
       </div>
     );
--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -1,10 +1,11 @@
 import React from "react";
 import UserItem from "./UserItem";
 import { Spinner } from "../layout/Spinnner";
-import PropTypes from 'prop-types';
+import PropTypes from "prop-types";
 
 export const Users = ({ users, loading }) => {
   if (loading) {
+    console.log('loading...');
     return <Spinner />;
   } else {
     return (
@@ -23,9 +24,14 @@ const userStyle = {
   gridGap: "1rem",
 };
 
+Users.defaultProps = {
+  users: [],
+  loading: false,
+};
+
 Users.propTypes = {
-  users:PropTypes.array.isRequired,
-  loading:PropTypes.bool.isRequired,
-}
+  users: PropTypes.array.isRequired,
+  loading: PropTypes.bool.isRequired,
+};
 
 export default Users;
